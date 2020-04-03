95874c51f6479eb558cfcdf8e423bec74979215f
mr-mateus <mateus395@gmail.com>
2020-04-03 01:04:59 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { RedirectComponent } from "./redirect/redirect.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full"
+  },
+  {
+    path: "login",
+    component: AppComponent
+  },
+  {
+    path: "token/:token?",
+    component: RedirectComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ng-callback';
+  email = "m";
+  password = "m";
+  constructor(private router: Router) {}
+
+  login() {
+    const login = {
+      email: this.email,
+      password: this.password
+    };
+    const loginBase64 = btoa(JSON.stringify(login));
+    // this.router.navigate(["token", loginBase64]);
+    const redirect = `${window.location.origin}/token/${loginBase64}`;
+    window.location.href = redirect;
+  }
 }
